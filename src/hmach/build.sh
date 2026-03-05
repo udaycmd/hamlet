ec66f73ad54de17d8cf3cd45add53671b10b1305
Uday Tiwari <udaytiwari720@gmail.com>
2026-03-05 16:41:30 +0530

    move vm to a seperate project

--- src/hmach/build.sh
+++
@@ -1,1 +0,0 @@
-clang++ -O3 -Wall -Wextra -o ./build/main ./src/main.cpp ./src/chunk.cpp ./src/opcode.cpp -I./src/include/
--- src/hmach/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "chunk.h"
-#include "opcode.h"
-
-int main() {
-	hmach::Chunk c;
-	c.write(hmach::OpCode::Return);
-	c.dis("test");
-}
